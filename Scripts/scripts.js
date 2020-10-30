9ded40edede97ccfb66f4ce1ebaa318d6ca880ad
Shemar Jones <47797339+S-TJones@users.noreply.github.com>
2020-10-29 22:09:12 -0500

    Update scripts.js
    Worked on the JS file. Used the DOM to get the required elements. Also added an onclick function on the form button, which checks to see if the data was entered or not. Haven't tested it as yet though.

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -1,1 +1,29 @@
 /* Add your JavaScript to this file */
+
+var driver = function(){
+    var first = "Thank you! your email address ";
+    var second = " has been added to our mailing list!";
+
+    // Locates the div "message"
+    var display = document.getElementsByClassName(message);
+
+    // Adds a onclick event listener to the button
+    display.addEventListener("click",function(){
+
+        // Gets the input field
+        var email_field = document.getElementsByTagName('input');
+        // Gets the data entered in the input field
+        var user_mail = email_field.value;
+
+        // Checks to see if no data was entered in the input field
+        if(user_mail === ""){
+            display.innerHTML = "ERROR";
+        }
+        else{
+            display.innerHTML = (first + user_mail + second);
+        }
+    });
+}
+
+// Runs this function on startup
+window.onload = driver;
