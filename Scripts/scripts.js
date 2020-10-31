8fe5275089fe4093825c656379c13389c113a4b3
Shemar Jones <47797339+S-TJones@users.noreply.github.com>
2020-10-31 17:39:58 -0500

    Completed the Javascript
    Made corrections to the javascript file. It now works as intended.

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -1,17 +1,22 @@
 /* Add your JavaScript to this file */
 
 var driver = function(){
-    var first = "Thank you! your email address ";
-    var second = " has been added to our mailing list!";
+    var first = "Thank you! your email address <";
+    var second = "> has been added to our mailing list!";
 
-    // Locates the div "message"
-    var display = document.getElementsByClassName(message);
+    // Locates the div "message" and button "btn"
+    var display = document.getElementsByClassName("message")[0];
+    var button = document.getElementsByClassName("btn")[1];
 
     // Adds a onclick event listener to the button
-    display.addEventListener("click",function(){
+    button.addEventListener("click", function(event){
+        
+        // The preventDefault() method cancels the event if it is cancelable, 
+        // meaning that the default action that belongs to the event will not occur.
+        event.preventDefault();
 
         // Gets the input field
-        var email_field = document.getElementsByTagName('input');
+        var email_field = document.getElementsByTagName("input")[0];
         // Gets the data entered in the input field
         var user_mail = email_field.value;
 
@@ -20,8 +25,11 @@ var driver = function(){
             display.innerHTML = "ERROR";
         }
         else{
+            // Sets the text in the div
             display.innerHTML = (first + user_mail + second);
         }
+
+        
     });
 }
 
